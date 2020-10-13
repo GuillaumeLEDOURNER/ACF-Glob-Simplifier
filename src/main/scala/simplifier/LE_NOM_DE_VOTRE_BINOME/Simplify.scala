ba4d8b5a6d0a731cd1ffff465a325d2116ec714c
Elodie Le Gal <elodi.le.gal@gmail.com>
2020-10-13 16:19:53 +0200

    implementation 1

--- src/main/scala/simplifier/LE_NOM_DE_VOTRE_BINOME/Simplify.scala
+++ src/main/scala/simplifier/LE_NOM_DE_VOTRE_BINOME/Simplify.scala
@@ -4,8 +4,34 @@ import library._
 
 class MySimplifier extends Simplifier{
 	def simplify(p: List[Symbol]): List[Symbol] = {
-		// TODO!!!
-		List()
+		p match {
+			case Nil => Nil
+			case Star :: Star :: tl =>  simplify(Star:: tl)
+			case Qmark :: Star :: tl => simplify(Plus  :: tl)
+			case Star :: Qmark :: tl => simplify(Plus :: tl)
+			case Qmark :: Plus :: tl => Qmark :: simplify(Plus :: tl)
+			case Plus :: Qmark :: tl => Plus :: simplify(Qmark :: tl)
+			case Star :: Plus :: tl => simplify(Plus :: tl)
+			case Plus :: Star :: tl => simplify(Plus :: tl)
+			case Plus :: Plus :: tl => Qmark :: simplify(Plus :: tl)
+			case Qmark :: Qmark :: tl => Qmark :: simplify(Qmark :: tl)
+
+			case Char(a) :: Plus :: tl => Char (a) :: simplify(Plus ::tl)
+			case Char(a) :: Star :: tl => Char (a) :: simplify(Star ::tl)
+			case Char(a) :: Qmark :: tl => Char (a) :: simplify(Qmark ::tl)
+
+			case Plus :: Char(a) :: tl => Plus :: simplify(Char(a) ::tl)
+			case Star :: Char(a) :: tl => Star :: simplify(Char(a) ::tl)
+			case Qmark :: Char(a) :: tl => Qmark :: simplify(Char(a) ::tl)
+
+			case Char (a) :: Char(b) :: tl => Char (a) :: Char (b) :: simplify(tl)
+
+			case Char (a) :: Nil => Char (a) :: Nil
+			case Star :: Nil => Star :: Nil
+			case Plus :: Nil => Plus :: Nil
+			case Qmark :: Nil => Qmark :: Nil
+			
+		}
 	}
 
 }
--- src/test/scala/main/TestSimplify.scala
+++ src/test/scala/main/TestSimplify.scala
@@ -14,4 +14,102 @@ class TestSimplify {
     
     assertEquals(pres, simp.simplify(p))
   }
+
+  @Test 
+  def t1(){
+    val simp = new MySimplifier
+    var p = List(Plus,Plus)
+    var pres = List (Qmark, Plus)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Star,Star)
+    var pres = List (Star)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Qmark,Qmark)
+    var pres = List (Qmark,Qmark)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Plus,Star)
+    var pres = List (Plus)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Star,Plus)
+    var pres = List (Plus)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Star,Qmark)
+    var pres = List (Plus)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Qmark, Star)
+    var pres = List (Plus)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Qmark, Plus)
+    var pres = List (Qmark, Plus)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Plus, Qmark)
+    var pres = List (Plus, Qmark)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Char(a),Plus)
+    var pres = List(Char(a),Plus)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Char(a),Star)
+    var pres = List(Char(a),Star)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Char(a),Qmark)
+    var pres = List(Char(a),Qmark)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Qmark,Char(a))
+    var pres = List(Qmark,Char(a))
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Star,Char(a))
+    var pres = List(Star,Char(a))
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Plus,Char(a))
+    var pres = List(Plus,Char(a))
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Char(a),Char(b))
+    var pres = List(Char(a),Char(b))
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Char(a),Nil)
+    var pres = List(Char(a),Nil)
+    assertEquals(pres, simp.simplify(p))
+  
+    var p = List(Star,Nil)
+    var pres = List(Star,Nil)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Plus,Nil)
+    var pres = List(Plus,Nil)
+    assertEquals(pres, simp.simplify(p))
+    
+    var p = List(Qmark,Nil)
+    var pres = List(Qmark,Nil)
+    assertEquals(pres, simp.simplify(p))
+  }
+
+  @Test 
+  def t2(){
+    val simp = new MySimplifier
+    var p = List(Plus,Char(c),Star,Qmark,Char(d),Star,Plus)
+    var pres = List (Plus,Char(c),Plus,Char(d),Plus)
+    assertEquals(pres, simp.simplify(p))
+
+    var p = List(Char(a),Star,Star)
+    var pres = List(Char(a),Star)
+    assertEquals(pres, simp.simplify(p))
+  }
+
+
 }
